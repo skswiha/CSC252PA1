d031fca8c6c056b878a26d13b25379351c0fa5a9
Sarah Swihart <skswiha@gmail.com>
2019-10-30 13:32:29 -0400

    Finished code for project

--- src/main/QuickSortPivotMedian.java
+++ src/main/QuickSortPivotMedian.java
@@ -1,59 +1,106 @@
 package main;
+import java.util.Arrays;
 
 public class QuickSortPivotMedian {
-	
-	int partition(int arr[], int low, int high) 
-	{ 
-		int pivot = arr[high]; 
-		int i = (low-1); // index of smaller element 
-		for (int j=low; j<high; j++) 
-		{ 
-			// If current element is smaller than the pivot 
-			if (arr[j] < pivot) 
-			{ 
-				i++; 
 
-				// swap arr[i] and arr[j] 
-				int temp = arr[i]; 
-				arr[i] = arr[j]; 
-				arr[j] = temp; 
-			} 
-		} 
+	public static int medianPivot(int arr[], int low, int high) {
+		/*
+		 * create subarray with low, high, and middle elements in the array sort the
+		 * subarray and use index 1 as the median of 3
+		 */
 
-		// swap arr[i+1] and arr[high] (or pivot) 
-		int temp = arr[i+1]; 
-		arr[i+1] = arr[high]; 
-		arr[high] = temp; 
+		int mid = (high) / 2;
 
-		return i+1; 
-	} 
+		//System.out.println("\tMiddle of Arr at Index= " + mid + " : " + arr[mid]);
+		int[] sortingArr = { arr[low], arr[mid], arr[high] };
 
+		Arrays.sort(sortingArr);
 
-	/* The main function that implements QuickSort() 
-	arr[] --> Array to be sorted, 
-	low --> Starting index, 
-	high --> Ending index */
-	void sort(int arr[], int low, int high) 
-	{ 
-		if (low < high) 
-		{ 
-			/* pi is partitioning index, arr[pi] is 
-			now at right place */
-			int pi = partition(arr, low, high); 
+		int middleValue = sortingArr[1];
+		//System.out.println("\t"+Arrays.toString(sortingArr));
+		//printArray(sortingArr);
 
-			// Recursively sort elements before 
-			// partition and after partition 
-			sort(arr, low, pi-1); 
-			sort(arr, pi+1, high); 
-		} 
-	} 
+		// swap with the last to serve as pivot
+		int temp = arr[high];
+		arr[high] = middleValue;
+		if (middleValue == arr[low]) {
+			arr[low] = temp;
+		} else if (middleValue == arr[mid]) {
+			arr[mid] = temp;
+		}
 
+		// System.out.println("median: ");
+		//printArray(arr);
+		return partition(arr, low, high);
+
+	}
+
+	// ----------------------------------------------------------------------
+	/*  method for medianQuicksort */
+	public static void medianQuickSort(int arr[], int low, int high) {
+		if (low >= high)
+			return;
+
+		if (low < high) {
+
+			int pi = medianPivot(arr, low, high);
+
+			QuickSort(arr, low, high);
+
+		}
+	}
+
+	// -----------------------------------------------------------------------
+	public static void QuickSort(int arr[], int low, int high) {
+
+		if (low < high) {
+			int pi = partition(arr, low, high);
+
+			// Recursively sort elements before
+			// partition and after partition
+			QuickSort(arr, low, pi - 1);
+			QuickSort(arr, pi + 1, high);
+		}
+	}
+
+	// ----------------------------------------------------------------------
 	/* A utility function to print array of size n */
-	static void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i]+" "); 
-		System.out.println(); 
-	} 
+	public static void printArray(int arr[]) {
+		int n = arr.length;
+		System.out.print("\tarr[] = { ");
+		for (int i = 0; i < n; ++i) {
+
+			System.out.print(arr[i] + " ");
+
+		}
+		System.out.print("}\n");
+	}
+
+	// -----------------------------------------------------------------------
+	public static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = (low - 1); // index of smaller element
+
+		for (int j = low; j < high; j++) {
+			// If current element is smaller than or
+			// equal to pivot
+			if (arr[j] <= pivot) {
+				i++;
+
+				// swap arr[i] and arr[j]
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+		return i + 1;
+
+	}
 }
+
+//code modified from https://gist.github.com/epomp447/4c0d0676d9f013788647cbe6e60ae0df
--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -4,25 +4,60 @@ import java.util.Random;
 
 public class TestMain {
 	public static void main(String[]args) {
-		int[] a = generateArray(8);
-		QuickSortPivotLast ob = new QuickSortPivotLast();
-		ob.sort(a, 0, a.length-1);
-		System.out.println();
-		System.out.print("Sorted Array: ");
-		QuickSortPivotLast.printArray(a);
-	
+		int[] a;
+		//Loop to test QuickSortPivotLast, uncomment to run
+		/*for (int i = 0; i < 20; i++) {
+			a = generateArray(100000);
+			QuickSortPivotLast ob = new QuickSortPivotLast();
+			long startTime = System.nanoTime();
+			ob.sort(a, 0, a.length-1);
+			long endTime = System.nanoTime();
+
+			double duration = (endTime - startTime) * 0.000001;
+			//System.out.println();
+			//System.out.print("Quick Sorted Array: ");
+			//QuickSortPivotLast.printArray(a);
+			System.out.println(duration);
+		}*/
+		//Loop to test QuickSortPivotMedian (uncomment to run
+		/*for (int i = 0; i < 20; i++) {
+			a = generateArray(100000);
+			long startTime = System.nanoTime();
+			QuickSortPivotMedian.medianQuickSort(a, 0, a.length-1);
+			long endTime = System.nanoTime();
+
+			double duration = (endTime - startTime) * 0.000001;
+			//System.out.println();
+			//System.out.print("Quick Sorted Array: ");
+			//QuickSortPivotLast.printArray(a);
+			System.out.println(duration);
+		} */
+		//Loop to test MergeSort
+		for (int i = 0; i < 20; i++) {
+			a = generateArray(50000);
+			MergeSort ob = new MergeSort();
+			long startTime = System.nanoTime();
+			ob.sort(a, 0, a.length-1);
+			long endTime = System.nanoTime();
+
+			double duration = (endTime - startTime) * 0.000001;
+			//System.out.println();
+			//System.out.print("Quick Sorted Array: ");
+			//QuickSortPivotLast.printArray(a);
+			System.out.println(duration);
+		}
 	}
 	
 	public static int[] generateArray(int arraySize) {
 		int[] a = new int [arraySize];
 		Random r = new Random();
 		for(int i = 0; i < arraySize; i++) {
-			a[i]=r.nextInt(100);
+			a[i]=r.nextInt();
 		}
-		System.out.print("Unsorted Array: ");
+		/*System.out.print("Unsorted Array: ");
 		for(int i = 0; i < arraySize; i++) {
 			System.out.print(a[i] + " ");
-		}
+		}*/
 		return a;
 	}
 }
